efa72f3b50c6c13ecae230cd456853d1b923d166
TommyD <tom.dato@gmail.com>
2021-01-12 12:48:32 -0600

    using a list

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace GradeBook
 {
@@ -6,7 +8,31 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1};
+            grades.Add(100.0);
+            var result = 0.0;
+            var average = 0.0;
+            foreach(double grade in grades) 
+            {
+                result += grade;
+            }
+            average = result / grades.Count;
+
+            Console.WriteLine(result);
+            Console.WriteLine($"Average grade is {average}");
+
+            if (args.Length > 0) 
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendJoin(' ', args);
+                Console.WriteLine($"Hello, {sb.ToString()}!");
+            } 
+            else 
+            {
+                Console.WriteLine("Hello!");
+            }
+
         }
     }
 }
