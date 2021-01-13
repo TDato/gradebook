016660f3d004a9bb03b6a467a851d528e9df3280
TommyD <tom.dato@gmail.com>
2021-01-13 08:33:33 -0600

    Added letter grade functionality

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -20,13 +20,46 @@ namespace GradeBook
             this.grades = new List<double>();
         }
 
+        public void AddGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                    AddGrade(60);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         /// <summary>
         /// Adds a double value to the grade list in the book
         /// </summary>
         /// <param name="grade">A double precision number.</param>
         public void AddGrade(double grade) 
         {
-            this.grades.Add(grade);
+            if(grade <= 100 && grade >= 0) 
+            {
+                this.grades.Add(grade);
+            }
+            else 
+            {
+                Console.WriteLine("Invalid value");
+            }
         }
         // /// <summary>
         // /// Computes the average grade value
@@ -64,6 +97,29 @@ namespace GradeBook
             }
 
             result.Average /= this.grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
